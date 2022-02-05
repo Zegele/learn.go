bb0641630fe7de39d075715fe44ed8cad60a334c
Nine <zegestudio@gmail.com>
2022-02-05 22:54:11 +0800

    2.5 channel（16课）

---
+++ chapter06/14.channel-with-direction/direction_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWithDirection(t *testing.T) {
+	c := make(chan int, 100)
+	inOnly(c)
+	outOnly(c)
+}
+
+func inOnly(c chan<- int) {
+	c <- 1
+	//<-c // 当c是单向（入）channel时，不能再取数。编译错误
+}
+
+func outOnly(c <-chan int) {
+	//c <- 1 // 当c是单向（出）channel时，不能再存入数据。编译错误
+	v := <-c
+	fmt.Println(v)
+}
