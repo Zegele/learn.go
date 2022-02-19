60f2c9e3246773251199a050066b73f39c8e991e
Nine <zegestudio@gmail.com>
2022-02-19 12:30:11 +0800

    2.19 补19课：算法：归并排序

---
+++ chapter07/07.sort.merge/merge.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func mergeSort(sli []int) []int {
+	left, right := sli[:len(sli)/2], sli[len(sli)/2:] //老师这样写：(sli)[:len(sli)/2],(sli)[len(sli)/2:]
+	if len(sli) <= 2 {
+		return mergeSli(left, right)
+	} else {
+		return mergeSli(mergeSort(left), mergeSort(right)) //递归了
+	}
+}
+
+func mergeSli(left, right []int) []int {
+	out := []int{}
+	leftI, rightI := 0, 0
+	for {
+		if leftI == len(left) || rightI == len(right) {
+			break
+		}
+		if left[leftI] < right[rightI] {
+			out = append(out, left[leftI])
+			leftI++
+			continue //进入下一次循环
+		} else {
+			out = append(out, right[rightI])
+			rightI++
+			continue
+		}
+	}
+	for ; leftI < len(left); leftI++ {
+		out = append(out, left[leftI])
+	}
+	for ; rightI < len(right); rightI++ {
+		out = append(out, right[rightI])
+	}
+	return out
+}
+
+func main() {
+	SliSize := 10
+	sli := []int{}
+	rand.Seed(time.Now().UnixNano()) //随机种子
+	for i := 0; i < SliSize; i++ {
+		sli = append(sli, rand.Intn(50))
+	}
+	//fmt.Println(sli)
+
+	start := time.Now()
+	/*sorted :=*/ mergeSort(sli)
+	finish := time.Now()
+	fmt.Println(finish.Sub(start))
+	//fmt.Println(sorted)
+}
